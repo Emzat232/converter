50ac92dc3177d1b9a5f32d5471d6255fd28b0df7
Emzat232 <161577048+Emzat232@users.noreply.github.com>
2025-12-03 15:45:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+	 function amount() {
+		 
+		if (document.getElementById("curr").value === "South African Rands") {
+			const conv = document.getElementById("amount").value;
+			let aconv = conv / 17.09;
+			aconv == aconv.toFixed(2);
+			document.getElementById("output1").textContent = `R ${conv} \= \$${aconv}`;
+			let bconv = conv / 19.93;
+			bconv == bconv.toFixed(2);
+			document.getElementById("output2").textContent = `R ${conv} \= \€${bconv}`;
+		}
+		
+		else if (document.getElementById("curr").value === "United States Dollars") {
+			const conv = document.getElementById("amount").value;
+			let aconv = conv * 0.86;
+			aconv == aconv.toFixed(2);
+			document.getElementById("output1").textContent = `$${conv} \= \€${aconv}`;
+			let bconv = conv * 17.07;
+			bconv == bconv.toFixed(2);
+			document.getElementById("output2").textContent = `$${conv} \= R${bconv}`;
+		}
+		
+		else if (document.getElementById("curr").value === "Euros") {
+			const conv = document.getElementById("amount").value;
+			let aconv = conv * 19.92;
+			aconv == aconv.toFixed(2);
+			document.getElementById("output1").textContent = `€${conv} \= \R${aconv}`;
+			let bconv = conv * 1.17;
+			bconv == bconv.toFixed(2);
+			document.getElementById("output2").textContent = `€${conv} \= \$${bconv}`;
+		}
+	 }
+	
